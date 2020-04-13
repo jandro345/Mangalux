838f215950ca19557f976f7ae85a3722a986f65c
jandro345 <alejandrocallejonreche@gmail.com>
2020-04-13 18:20:58 +0200

    Segunda versión: -Panel de búsqueda creado -Panel de historial creado -Panel de inicio creado

--- app/src/main/java/com/example/probando_1/MainActivity.java
+++ app/src/main/java/com/example/probando_1/MainActivity.java
@@ -15,18 +15,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Botón de búsqueda
         ImageButton busqueda=findViewById(R.id.busqueda);
         busqueda.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                entrada(v);
+                entrada1(v);
+            }
+        });
+
+        //Botón de Historial
+        ImageButton historial=findViewById(R.id.historial);
+        historial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                entrada2(v);
+            }
+        });
+
+        //Botón de Inicio
+        ImageButton inicio=findViewById(R.id.inicio);
+        inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                entrada3(v);
             }
         });
     }
-    void entrada(View view){
+
+    //Función que salta a la actividad de inicio
+    void entrada3(View view){
+        Intent Random;
+        Random= new Intent(this,inicio.class);
+        startActivity(Random);
+    }
+
+    //Función que salta a la actividad de buscar
+    void entrada2(View view){
         Intent Random;
         Random= new Intent(this,buscar.class);
+        startActivity(Random);
+    }
+    //Función que salta a la actividad de historial
+    void entrada1(View view){
+        Intent Random;
+        Random= new Intent(this,historial.class);
         startActivity(Random);
 
     }
--- app/src/main/java/com/example/probando_1/historial.java
+++ app/src/main/java/com/example/probando_1/historial.java
@@ -3,16 +3,12 @@ package com.example.probando_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.SearchView;
 
-public class buscar extends AppCompatActivity {
+public class historial extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_buscar);
-
-
-
+        setContentView(R.layout.activity_historial);
     }
 }
